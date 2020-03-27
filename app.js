ddc3942858dcad431d26890240fa5bb32e063520
apc <alexcurran.va@gmail.com>
2020-03-27 16:10:28 -0500

    Re-usuable web components!

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+customElements.define("my-paragraph",
+    class extends HTMLElement {
+        constructor() {
+            super();
+
+            const template = document.getElementById("my-paragraph");
+            const templateContent = template.content;
+            const shadowRoot = this
+                .attachShadow({ mode: "open" })
+                .append(templateContent.cloneNode(true));
+        }
+    }
+
+)
